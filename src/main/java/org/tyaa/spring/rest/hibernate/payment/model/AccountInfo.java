48b6eda8a9b5d0ec73e034cdb6d6038c801a38c5
YuriiTrofimenko <tyaa@ukr.net>
2018-10-18 12:17:46 +0300

    init

---
+++ src/main/java/org/tyaa/spring/rest/hibernate/payment/model/AccountInfo.java
@@ -0,0 +1,16 @@
+package org.tyaa.spring.rest.hibernate.payment.model;
+
+public class AccountInfo {
+
+	public String userName;
+
+	public AccountInfo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AccountInfo(String userName) {
+		super();
+		this.userName = userName;
+	}
+}
---
+++ src/main/java/org/tyaa/spring/rest/hibernate/payment/service/AuthService.java
@@ -0,0 +1,87 @@
+package org.tyaa.spring.rest.hibernate.payment.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.tyaa.spring.rest.hibernate.payment.dao.CategoryHibernateDAO;
+import org.tyaa.spring.rest.hibernate.payment.dao.RoleHibernateDAO;
+import org.tyaa.spring.rest.hibernate.payment.dao.UserHibernateDAO;
+import org.tyaa.spring.rest.hibernate.payment.entity.Category;
+import org.tyaa.spring.rest.hibernate.payment.entity.Role;
+import org.tyaa.spring.rest.hibernate.payment.entity.User;
+import org.tyaa.spring.rest.hibernate.payment.model.AbstractResponse;
+import org.tyaa.spring.rest.hibernate.payment.model.AccountInfo;
+import org.tyaa.spring.rest.hibernate.payment.model.AccountInfoResponse;
+import org.tyaa.spring.rest.hibernate.payment.model.CategoryResponse;
+import org.tyaa.spring.rest.hibernate.payment.model.UserRequest;
+
+@Service
+@Transactional
+public class AuthService {
+
+	@Autowired
+	private RoleHibernateDAO roleDao;
+	
+	@Autowired
+	private UserHibernateDAO userDao;
+
+	public AbstractResponse<Object> createRole(Role _role) {
+		
+		roleDao.create(_role);
+		AbstractResponse<Object> response =
+				new AbstractResponse<Object>() {};
+		response.setStatus("success");
+		response.setMessage(
+			String.format("Role %s created", _role.getName())
+		);
+		return response;
+	}
+
+	public AbstractResponse<Object> createUser(UserRequest _userRequest) {
+		
+		Role role = roleDao.get(_userRequest.getRole_id());
+		User newUser =
+			userDao.create(
+				new User(
+					_userRequest.getName()
+					, _userRequest.getPassword()
+					, role
+					)
+				);
+		AbstractResponse<Object> response =
+				new AbstractResponse<Object>() {};
+		response.setStatus("success");
+		response.setMessage(
+			String.format("User %s created", newUser.getName())
+		);
+		return response;
+	}
+	
+	public AbstractResponse getAccountInfo(UserRequest _userRequest) {
+		
+		AbstractResponse response =
+				new AbstractResponse<Object>() {};
+		User user =
+			userDao.getUserByName(_userRequest.getName());
+		if(user == null) {
+			
+			response.setStatus("error");
+			response.setMessage(
+				String.format("User %s not found", _userRequest.getName())
+			);
+		} else {
+			
+			AccountInfoResponse successResponse = new AccountInfoResponse();
+			successResponse.setStatus("success");
+			successResponse.setMessage(
+				String.format("User %s found", _userRequest.getName())
+			);
+			successResponse.setAccountInfo(new AccountInfo(user.getName()));
+		}
+		
+		return response;
+	}
+}
